3410c5fff654652e26e9e2418783390840b245a4
ShashwatDevtron <shashwat@devtron.ai>
2023-01-19 21:35:40 +0530

    docker added

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func main(){
 	r := mux.NewRouter()
 	routes.RegisterAttendanceMangementRoutes(r)
 	http.Handle("/",r)
-	log.Fatal(http.ListenAndServe("localhost:9010",r))
+	log.Fatal(http.ListenAndServe(":9010",r))
 }
